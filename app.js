4e875d0b41ba29dfde2744f2a7af0c9689df2ecd
aradhanasinghmcscet <aradhanasinghmcscet@gmail.com>
2025-08-20 00:59:53 +0530

    service updated

--- app.js
+++ app.js
@@ -2,17 +2,24 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const patientRoutes = require('./routes/patientRoutes');
-
 
 const app = express();
 const PORT = 4002;
+
+// Enable CORS for all routes
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(bodyParser.json());
 console.log(process.env.JWT_SECRET)
 app.use('/patients', patientRoutes);
 
 app.listen(PORT, () => {
-  
   console.log(`Server running on http://localhost:${PORT}`);
 });
